63e3dd626eceeb98bd6f31493ae5e3384424c522
Pranjul Agarwal <51283364+PranjulAgarwal@users.noreply.github.com>
2021-04-20 19:22:12 +0530

    Create wishlist_button_18.js

---
+++ wishlist_button_18.js
@@ -0,0 +1,48 @@
+console.log("From CDN Script");
+
+const wish_button = `<style>
+.button_wish {
+  background-color: ${colorBefore};
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+<button class="button_wish">${labelBefore}</button>`
+
+function init() {
+  console.log("from init function")
+    document
+        .querySelector("div.product-single__description")
+        .insertAdjacentHTML("beforebegin", wish_button);
+}
+
+const serverUrl = "https://dd9bb97e4385.ngrok.io";
+
+fetch(`${serverUrl}/db/save/${window.location.hostname}`).then(function (response) {
+	// The API call was successful!
+	return response.json();
+}).then(function (data) {
+	// This is the JSON from our response
+	console.log(data);
+    var Active = data.Active;
+    var Value = data.ButtonType;
+    var colorBefore = data.ColorBeforeButton;
+    var colorAfter = data.ColorAfterButton;
+    var labelBefore = data.LabelBeforeButton;
+    var labelAfter = data.LabelAfterButton;
+    var CartLabelBefore = data.CartLabelBefore;
+    var CartLabelAfter = data.CartLabelAfter;
+    var color = data.CartColor;
+    var Count = data.ShowCount;
+    init();
+}).catch(function (err) {
+	// There was an error
+	console.warn('Something went wrong.', err);
+});
