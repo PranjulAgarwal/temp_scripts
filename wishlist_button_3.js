258087eb007a4671fa36d0dfc5b8b1d4ecae276b
Pranjul Agarwal <51283364+PranjulAgarwal@users.noreply.github.com>
2021-04-20 15:23:34 +0530

    Create wishlist_button_3.js

---
+++ wishlist_button_3.js
@@ -0,0 +1,21 @@
+const wish_button = `<style>
+.button_wish {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+<button class="button_wish">Button</button>`
+
+function init() {
+    console.log("In 3");
+      document
+        .querySelector("footer")
+        .insertAdjacentHTML("beforebegin", wish_button);
