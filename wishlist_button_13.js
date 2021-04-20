6f795393f9da91ee4b13d158f3dad698c10a6bf7
Pranjul Agarwal <51283364+PranjulAgarwal@users.noreply.github.com>
2021-04-20 18:41:30 +0530

    Create wishlist_button_13.js

---
+++ wishlist_button_13.js
@@ -0,0 +1,37 @@
+console.log("From 12 new");
+
+const wish_button = `<style>
+.button_wish {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+<button class="button_wish">Button</button>`
+
+function init() {
+  console.log("from script 12")
+    document
+        .querySelector("div.product-single__description")
+        .insertAdjacentHTML("beforebegin", wish_button);
+}
+
+const serverUrl = "https://39c8a2397cd7.ngrok.io";
+
+fetch(`${serverUrl}/db/save/${window.location.hostname}`).then(function (response) {
+	// The API call was successful!
+  // console.log(response);
+	// This is the JSON from our response
+	console.log(response.data.json());
+    init();
+}).catch(function (err) {
+	// There was an error
+	console.warn('Something went wrong.', err);
+});
