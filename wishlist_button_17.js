6e5242f36f702ab4a5bb0a165fd9e35f2a9f2a52
Pranjul Agarwal <51283364+PranjulAgarwal@users.noreply.github.com>
2021-04-20 19:08:23 +0530

    Create wishlist_button_17.js

---
+++ wishlist_button_17.js
@@ -0,0 +1,38 @@
+console.log("From CDN Script");
+
+const wish_button = `<style>
+.button_wish {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+<button class="button_wish">Button</button>`
+
+function init() {
+  console.log("from init function")
+    document
+        .querySelector("div.product-single__description")
+        .insertAdjacentHTML("beforebegin", wish_button);
+}
+
+const serverUrl = "https://dd9bb97e4385.ngrok.io";
+
+fetch(`${serverUrl}/db/save/${window.location.hostname}`).then(function (response) {
+	// The API call was successful!
+	return response.json();
+}).then(function (data) {
+	// This is the JSON from our response
+	console.log(data);
+    init();
+}).catch(function (err) {
+	// There was an error
+	console.warn('Something went wrong.', err);
+});
