f9c2e329dac843dd0a52886f3347db4fc9b6a518
Pranjul Agarwal <51283364+PranjulAgarwal@users.noreply.github.com>
2021-04-20 17:12:18 +0530

    Update wishlist_button_5.js

--- wishlist_button_5.js
+++ wishlist_button_5.js
@@ -1,2 +1,26 @@
  console.log("From 5");
 
+const wish_button = `<style>
+.button_wish {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+<button class="button_wish">Button</button>`
+
+function init() {
+  console.log("from script 5")
+    document
+        .querySelector("div.product-form__item product-form__item--submit product-form__item--payment-button")
+        .insertAdjacentHTML("afterend", wish_button);
+}
+
+init();
